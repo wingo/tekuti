c631aaf35c9a9158ba5032598be8216891f1be76
Andy Wingo <wingo@pobox.com>
2014-10-15 09:46:53 +0200

    Allow scheme-relative URIs in urlish?
    * tekuti/util.scm (urlish?): Allow scheme-relative URIs.

--- tekuti/util.scm
+++ tekuti/util.scm
@@ -1,5 +1,5 @@
 ;; Tekuti
-;; Copyright (C) 2008, 2010, 2011, 2012 Andy Wingo <wingo at pobox dot com>
+;; Copyright (C) 2008, 2010, 2011, 2012, 2014 Andy Wingo <wingo at pobox dot com>
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -62,7 +62,7 @@
 (define (urlish? x)
   (let ((uri (string->uri x)))
     (and uri
-         (memq (uri-scheme uri) '(http https))
+         (memq (uri-scheme uri) '(#f http https))
          (uri-host uri)
          #t)))
 
